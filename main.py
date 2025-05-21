ed28b637ca0427ea416f53d8d730efc01aadd90f
nbarlowATI <nbarlow@turing.ac.uk>
2025-05-21 18:19:15 +0100

    3d view with textures

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from settings import *
 from map import *
 from player import *
+from raycasting import *
+from object_renderer import *
 
 class Game:
     def __init__(self):
@@ -15,18 +17,22 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
+        self.raycasting = RayCasting(self)
         pass
 
     def update(self):
         self.player.update()
+        self.raycasting.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
 
     def draw(self):
         self.screen.fill("black")
-        self.map.draw()
-        self.player.draw()
+        self.object_renderer.draw()
+ #       self.map.draw()
+ #       self.player.draw()
 
 
     def check_events(self):
