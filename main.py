5bd7e51cc8518eb11d41fba5317eac138caeaf2b
nbarlowATI <nbarlow@turing.ac.uk>
2025-05-21 09:03:26 +0100

    finish prep stage

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
@@ -36,6 +37,7 @@ class Game:
 
     def run(self):
         while True:
+            self.check_events()
             self.update()
             self.draw()
 
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # game settings
 RES = WIDTH, HEIGHT = 1600, 900
-FPS = 60
+FPS = 0
 
 PLAYER_POS = 1.5, 5
 PLAYER_ANGLE = 0
